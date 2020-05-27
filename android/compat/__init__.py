ivision-research/banjo

commit ad341d7a9be0ae8b467bd6f4cd4e6e85e69c2128

Improve typing
There are still errors around the pseudoinstructions being a dict with
a union value.

android/compat/__init__.py
@@ -21,12 +21,14 @@ except ModuleNotFoundError:
     def log_warn(s: str) -> None:
         print(s)
 
-    class Endianness(enum.Enum):
+    # When https://github.com/python/mypy/issues/1153 is fixed, these type
+    # ignores should be removed
+    class Endianness(enum.Enum):  # type: ignore
         BigEndian = enum.auto()
         LittleEndian = enum.auto()
 
     @dataclass
-    class InstructionTextTokenType:
+    class InstructionTextTokenType:  # type: ignore
         # Text that doesn't fit into the other tokens
         TextToken = enum.auto
         # The instruction mnemonic
@@ -47,7 +49,7 @@ except ModuleNotFoundError:
         FloatingPointToken = enum.auto
 
     @dataclass
-    class InstructionTextToken:
+    class InstructionTextToken:  # type: ignore
         token_type: InstructionTextTokenType
         text: str
         value: int = 0
@@ -56,7 +58,7 @@ except ModuleNotFoundError:
         context = None
         address: int = 0
         confidence: int = 255
-        typeNames = []
+        typeNames: List[Any] = []
         width: int = 0
 
 

android/dex.py
@@ -7,14 +7,15 @@ from struct import pack, unpack
 from typing import Any, Dict, List, NewType, Optional, Tuple, Union, cast
 
 from .smali import (
-    disassemble_pseudoinstructions,
-    SmaliPackedSwitchPayload,
+    PseudoInstructions,
     SmaliFillArrayDataPayload,
+    SmaliPackedSwitchPayload,
     SmaliSparseSwitchPayload,
+    disassemble_pseudoinstructions,
 )
 
 try:
-    from compat import Endianness, log_debug, log_error, log_warn
+    from compat import Endianness, log_debug, log_error, log_warn  # type: ignore
 except ModuleNotFoundError:
     from .compat import Endianness, log_debug, log_error, log_warn
 
@@ -643,7 +644,7 @@ class DexFile(object):
         elif endian_bytes == b"\x78\x56\x34\x12":
             self.endianness = Endianness.LittleEndian
         else:
-            raise ValueError(f'Invalid endianness found: {endian_bytes!r}')
+            raise ValueError(f"Invalid endianness found: {endian_bytes!r}")
         if self.endianness == Endianness.BigEndian:
             # It is likely that these do not exist at all, but who knows
             log_warn(
@@ -1027,14 +1028,7 @@ class DexFile(object):
         self, data: bytes, size: int, offset_to_section: FileOffset
     ) -> None:
         self.code_items: Dict[FileOffset, DexCodeItem] = dict()
-        self.pseudoinstructions: Dict[
-            FileOffset,
-            Union[
-                SmaliPackedSwitchPayload,
-                SmaliFillArrayDataPayload,
-                SmaliSparseSwitchPayload,
-            ],
-        ] = dict()
+        self.pseudoinstructions: PseudoInstructions = cast(PseudoInstructions, dict())
         i = 0
         for num in range(size):
             code_item_off: FileOffset = cast(FileOffset, offset_to_section + i)
@@ -1128,7 +1122,9 @@ class DexFile(object):
 
             # Disassemble code to parse out pseudoinstruction data blocks
             self.pseudoinstructions.update(
-                disassemble_pseudoinstructions(code_item.insns, code_item_off + 16)
+                disassemble_pseudoinstructions(
+                    code_item.insns, cast(FileOffset, code_item_off + 16)
+                )
             )
 
             # code_items are 4-byte aligned
@@ -1150,9 +1146,9 @@ class DexFile(object):
             value_arg: int,
             struct_type: str,
             data: bytes,
-            lookup: List[Any] = None,
+            lookup: Optional[List[Any]] = None,
         ) -> Tuple[DexValue, int]:
-            if lookup:
+            if lookup is not None:
                 return (
                     DexValue(
                         type_=value_type,

android/generate_instruction_info.py
@@ -9,7 +9,11 @@ from pickle import dump
 from typing import Dict, List, Optional, SupportsInt, Tuple, Union, cast
 
 try:
-    from .smali import SmaliInstructionFormat, SmaliInstructionInfo, INSTRUCTIONS_PICKLE_PATH
+    from .smali import (
+        SmaliInstructionFormat,
+        SmaliInstructionInfo,
+        INSTRUCTIONS_PICKLE_PATH,
+    )
 except ModuleNotFoundError:
     from smali import (  # type: ignore
         SmaliInstructionFormat,
@@ -60,7 +64,7 @@ class TableParser(HTMLParser):
             self.cell += data
 
 
-def parse_syntax(cell: str, opcode: int = None) -> Tuple[str, str]:
+def parse_syntax(cell: str, opcode: Optional[int] = None) -> Tuple[str, str]:
     if cell == "(unused)":
         return "", ""
     elif opcode is None:
@@ -84,7 +88,9 @@ def parse_syntax(cell: str, opcode: int = None) -> Tuple[str, str]:
 
 
 def parse_row(
-    row: List[str], formats: Dict[str, SmaliInstructionFormat], opcode: int = None
+    row: List[str],
+    formats: Dict[str, SmaliInstructionFormat],
+    opcode: Optional[int] = None,
 ) -> SmaliInstructionInfo:
     formatid = row[0].split(" ")[1]
     mnemonic, syntax = parse_syntax(row[1], opcode)

android/smali.py
@@ -4,10 +4,20 @@ import re
 import unittest
 from pathlib import Path
 from struct import unpack
-from typing import TYPE_CHECKING, Any, Dict, List, Match, Tuple, Union, cast
+from typing import (
+    NewType,
+    TYPE_CHECKING,
+    Any,
+    Dict,
+    List,
+    Match,
+    Tuple,
+    Union,
+    cast,
+)
 
 try:
-    from compat import (
+    from compat import (  # type: ignore
         log_debug,
         log_error,
         log_warn,
@@ -99,6 +109,19 @@ class SmaliFillArrayDataPayload:
     element_width: int  # ushort
     size: int  # uint
     data: bytes  # ubyte
+
+
+PseudoInstructions = NewType(
+    "PseudoInstructions",
+    Dict[
+        "FileOffset",
+        Union[
+            SmaliPackedSwitchPayload,
+            SmaliFillArrayDataPayload,
+            SmaliSparseSwitchPayload,
+        ],
+    ],
+)
 
 
 def slice_nibbles(data: bytes, start_nibble: int, size: int = 1) -> int:
@@ -323,7 +346,7 @@ class TestFormattingArgsWithSyntax(unittest.TestCase):
 
 
 def tokenize_syntax(
-    df: 'DexFile', word: str, args: Dict[str, int]
+    df: "DexFile", word: str, args: Dict[str, int]
 ) -> List[InstructionTextToken]:
     tokens = list()
     tokens.append(InstructionTextToken(InstructionTextTokenType.TextToken, " "))
@@ -492,11 +515,12 @@ def tokenize_syntax(
 
 
 def disassemble(
-    df: 'DexFile', data: bytes, addr: 'FileOffset'
+    df: "DexFile", data: bytes, addr: "FileOffset"
 ) -> Tuple[List[InstructionTextToken], int]:
     # Static variable
     if "insns" not in disassemble.__dict__:
-        disassemble.insns = load_insns()
+        # https://github.com/python/mypy/issues/708
+        disassemble.insns = load_insns()  # type: ignore[attr-defined]
 
     if len(data) < 2:
         log_warn(
@@ -517,8 +541,9 @@ def disassemble(
             text += "    .end packed-switch"
         elif data[1] == 2:
             # sparse-switch
+            # FIXME why do these casts not work?
             ps = cast(SmaliSparseSwitchPayload, df.pseudoinstructions[addr])
-            text = f".sparse-switch\n"
+            text = ".sparse-switch\n"
             text += "".join(
                 [
                     f"        {hex(ps.keys[i])} -> :sswitch_offset_{ps.targets[i]:x}\n"
@@ -530,7 +555,7 @@ def disassemble(
             ps = cast(SmaliFillArrayDataPayload, df.pseudoinstructions[addr])
             text = f"pseudo-instruction: {ps}"
         else:
-            raise ValueError(f'Invalid pseudo-instruction with type {data[1]}')
+            raise ValueError(f"Invalid pseudo-instruction with type {data[1]}")
         return (
             [
                 InstructionTextToken(
@@ -542,7 +567,7 @@ def disassemble(
 
     # Now handle normal instructions
     tokens = list()
-    insn_info = disassemble.insns[data[0]]
+    insn_info = disassemble.insns[data[0]]  # type: ignore[attr-defined]
     tokens.append(
         InstructionTextToken(
             InstructionTextTokenType.InstructionToken, insn_info.mnemonic
@@ -607,18 +632,13 @@ def disassemble(
 
 
 def disassemble_pseudoinstructions(
-    data: bytes, addr: int
-) -> Dict[
-    int,
-    Union[
-        SmaliPackedSwitchPayload, SmaliFillArrayDataPayload, SmaliSparseSwitchPayload,
-    ],
-]:
+    data: bytes, addr: "FileOffset"
+) -> PseudoInstructions:
     # Static variable
     if "insns" not in disassemble.__dict__:
-        disassemble.insns = load_insns()
+        disassemble.insns = load_insns()  # type: ignore[attr-defined]
 
-    pseudoinstructions = dict()
+    pseudoinstructions: PseudoInstructions = cast(PseudoInstructions, dict())
     code_offset = 0
     while code_offset < len(data):
         if data[code_offset + 1] == 0 and data[code_offset] != 0:
@@ -629,7 +649,9 @@ def disassemble_pseudoinstructions(
             if data[code_offset] == 1:
                 # packed-switch-payload
                 size = unpack("<H", data_swapped[:2])[0]
-                pseudoinstructions[addr + code_offset] = SmaliPackedSwitchPayload(
+                pseudoinstructions[
+                    cast("FileOffset", addr + code_offset)
+                ] = SmaliPackedSwitchPayload(
                     _total_size=size * 4 + 8,
                     size=size,
                     first_key=unpack("<i", data_swapped[2:6])[0],
@@ -642,7 +664,9 @@ def disassemble_pseudoinstructions(
             elif data[code_offset] == 2:
                 # sparse-switch-payload
                 size = unpack("<H", data_swapped[:2])[0]
-                pseudoinstructions[addr + code_offset] = SmaliSparseSwitchPayload(
+                pseudoinstructions[
+                    cast("FileOffset", addr + code_offset)
+                ] = SmaliSparseSwitchPayload(
                     _total_size=size * 8 + 4,
                     size=size,
                     keys=[
@@ -659,7 +683,9 @@ def disassemble_pseudoinstructions(
                 # fill-array-data-payload
                 element_width = unpack("<H", data_swapped[:2])[0]
                 size = unpack("<I", data_swapped[2:6])[0]
-                pseudoinstructions[addr + code_offset] = SmaliFillArrayDataPayload(
+                pseudoinstructions[
+                    cast("FileOffset", addr + code_offset)
+                ] = SmaliFillArrayDataPayload(
                     _total_size=((size * element_width + 1) // 2) * 2 + 8,
                     element_width=element_width,
                     size=size,
@@ -673,7 +699,7 @@ def disassemble_pseudoinstructions(
                 code_offset += 2
         else:
             # Normal instruction
-            insn_info = disassemble.insns[data[code_offset + 1]]
+            insn_info = disassemble.insns[data[code_offset + 1]]  # type: ignore[attr-defined]
             code_offset += insn_info.fmt.insn_len * 2
     return pseudoinstructions
 

architecture.py
@@ -1,6 +1,7 @@
-import time
+"""Architecture class for Binary Ninja plugin."""
 import pickle
-from typing import Any, List, Tuple
+import time
+from typing import List, Tuple, cast
 
 from binaryninja.architecture import Architecture  # type: ignore
 from binaryninja.enums import BranchType  # type: ignore
@@ -9,10 +10,18 @@ from binaryninja.function import (  # type: ignore
     InstructionTextToken,
     RegisterInfo,
 )
-from binaryninja.lowlevelil import LowLevelILFunction, LowLevelILOperation, LLIL_TEMP  # type: ignore
+from binaryninja.lowlevelil import (  # type: ignore
+    LLIL_TEMP,
+    LowLevelILFunction,
+    LowLevelILOperation,
+)
 
 from .android.compat import log_debug, log_error, log_warn
+from .android.dex import DexFile, FileOffset
 from .android.smali import (
+    SmaliFillArrayDataPayload,
+    SmaliPackedSwitchPayload,
+    SmaliSparseSwitchPayload,
     disassemble,
     endian_swap_shorts,
     load_insns,
@@ -21,7 +30,21 @@ from .android.smali import (
 )
 
 
-class Smali(Architecture):
+class Smali(Architecture):  # type: ignore
+    """Architecture class for disassembling Dalvik bytecode into Smali
+
+    Initializing the class calls android.smali.load_insns(), which imports
+    cached instruction information from "android/instruction_data.pickle".
+
+    The three mandatory Architecture functions are implemented:
+        - get_instruction_info
+        - get_instruction_text
+        - get_instruction_low_level_il
+
+    There is also load_dex(), which is called the first time any of the three
+    functions are called. It loads the parsed DexFile class from disk.
+    """
+
     name = "Smali"
 
     # FIXME there should be 65536 registers, but binja hangs when the number gets above a thousand or so
@@ -37,10 +60,11 @@ class Smali(Architecture):
 
     def __init__(self) -> None:
         self.insns = load_insns()
-        self.df: Any = None
+        self.inialized_df: bool = False
         super().__init__()
 
     def load_dex(self) -> None:
+        """Load DexFile from disk. Should only be called once."""
         # FIXME all tabs in a window share the same Architecture class,
         # apparently. This means that, as far as I know, there's no way to
         # store this information per-tab. This could be hacked around if there
@@ -60,14 +84,15 @@ class Smali(Architecture):
             try:
                 # FIXME don't hardcode filename. See previous comment
                 with open("/tmp/out.json.pickle", "br") as f:
-                    self.df = pickle.load(f)
+                    self.df: DexFile = pickle.load(f)
+                    self.inialized_df = True
                     break
             except FileNotFoundError:
                 log_debug("Dex file decoding not finished. Sleeping")
                 time.sleep(1)
 
-    def get_instruction_info(self, data: bytes, addr: int) -> InstructionInfo:
-        if self.df is None:
+    def get_instruction_info(self, data: bytes, addr: FileOffset) -> InstructionInfo:
+        if not self.inialized_df:
             self.load_dex()
         ii = InstructionInfo()
 
@@ -125,7 +150,7 @@ class Smali(Architecture):
             )
         elif insn_info.mnemonic.startswith("invoke-"):
             if insn_info.mnemonic.startswith("invoke-custom"):
-                log_warn(f"Resolution of invoke-custom is not implemented")
+                log_warn("Resolution of invoke-custom is not implemented")
                 ii.add_branch(BranchType.UnresolvedBranch)
             else:
                 data_to_parse = endian_swap_shorts(data[: 2 * insn_info.fmt.insn_len])
@@ -136,25 +161,28 @@ class Smali(Architecture):
         return ii
 
     def get_instruction_text(
-        self, data: bytes, addr: int
+        self, data: bytes, addr: FileOffset
     ) -> Tuple[List[InstructionTextToken], int]:
-        if self.df is None:
+        if not self.inialized_df:
             self.load_dex()
         return disassemble(self.df, data, addr)
 
     def get_instruction_low_level_il(
-        self, data: bytes, addr: int, il: LowLevelILFunction
+        self, data: bytes, addr: FileOffset, il: LowLevelILFunction
     ) -> int:
-        if self.df is None:
+        if not self.inialized_df:
             self.load_dex()
         insn_info = self.insns[data[0]]
         if data[0] == 0x2B or data[0] == 0x2C and False:
             data_to_parse = endian_swap_shorts(data[: 2 * insn_info.fmt.insn_len])
             args = parse_with_format(data_to_parse, insn_info.fmt.format_)
             offset = sign(args["B"], insn_info.fmt.format_.count("B"))
-            payload = self.df.pseudoinstructions[addr + offset * 2]
             branches = list()  # [addr + offset * 2, addr + insn_info.fmt.insn_len * 2]
             if data[0] == 0x2B:  # packed-switch
+                payload = cast(
+                    SmaliPackedSwitchPayload,
+                    self.df.pseudoinstructions[cast(FileOffset, addr + offset * 2)],
+                )
                 for i in range(len(payload.targets)):
                     key = payload.first_key + i
                     target_addr = addr + payload.targets[i] * 2

binaryview.py
@@ -1,3 +1,6 @@
+"""BinaryView class for Binary Ninja plugin."""
+import base64
+import dataclasses
 import json
 import pickle
 from typing import Any, Set
@@ -14,11 +17,21 @@ from binaryninja.platform import Platform  # type: ignore
 from binaryninja.plugin import BackgroundTaskThread  # type: ignore
 from binaryninja.types import Symbol  # type: ignore
 
-from .android.dex import DexFile
-from .jsonencoder import DataclassJSONEncoder
+from .android.dex import AccessFlag, DexFile
 
 
-class JsonWriter(BackgroundTaskThread):
+class DataclassJSONEncoder(json.JSONEncoder):
+    def default(self, o: object) -> Any:
+        if isinstance(o, bytes):
+            return base64.b64encode(o).decode()
+        if dataclasses.is_dataclass(o):
+            return dataclasses.asdict(o)
+        if isinstance(o, AccessFlag):
+            return o.value
+        return super().default(o)
+
+
+class JsonWriter(BackgroundTaskThread):  # type: ignore
     def __init__(self, obj: Any, fn: str) -> None:
         BackgroundTaskThread.__init__(self, "Writing Dex structure as JSON")
         self.obj = obj
@@ -31,7 +44,7 @@ class JsonWriter(BackgroundTaskThread):
             json.dump(self.obj.__dict__, f, cls=DataclassJSONEncoder)
 
 
-class DexParser(BackgroundTaskThread):
+class DexParser(BackgroundTaskThread):  # type: ignore
 
     progress_title = "Parsing Dex"
 
@@ -121,7 +134,7 @@ class DexParser(BackgroundTaskThread):
         # TODO create data sections for static fields
 
 
-class Dex(BinaryView):
+class Dex(BinaryView):  # type: ignore
     name = "Dex"
 
     DEX_FILE_MAGICS = [b"dex\n039\0", b"dex\n038\0", b"dex\n037\0", b"dex\n035\0"]

jsonencoder.py
@@ -1,20 +1,0 @@
-import base64
-import dataclasses
-import json
-from typing import Any
-
-try:
-    from .android.dex import AccessFlag
-except ImportError:
-    from android.dex import AccessFlag
-
-
-class DataclassJSONEncoder(json.JSONEncoder):
-    def default(self, o: object) -> Any:
-        if isinstance(o, bytes):
-            return base64.b64encode(o).decode()
-        if dataclasses.is_dataclass(o):
-            return dataclasses.asdict(o)
-        if isinstance(o, AccessFlag):
-            return o.value
-        return super().default(o)

commit 7afc405f78cfe131218ee40cdf9aaecaa70a9164

Fix InstructionTextToken in compat
I'm not sure why it worked before or why ad341d7a9b broke it, but it's
fixed now.

android/compat/__init__.py
@@ -1,5 +1,5 @@
 import enum
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 from typing import Any, List
 
 try:
@@ -58,7 +58,7 @@ except ModuleNotFoundError:
         context = None
         address: int = 0
         confidence: int = 255
-        typeNames: List[Any] = []
+        typeNames: List[Any] = field(default_factory=list)
         width: int = 0
 
 